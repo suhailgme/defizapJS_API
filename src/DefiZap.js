c5e3e88cbd2f03cb85137526dc0814c4f1dc38bf
SuhailG <github@suhailg.me>
2019-12-01 23:27:04 -0700

    Add state bool var to track whether web3/metamask is in use

--- src/DefiZap.js
+++ src/DefiZap.js
@@ -6,9 +6,11 @@ module.exports =  class DefiZap {
     constructor(provider){
         if(provider){
             this.currentProvider = new ethers.providers.Web3Provider(provider.currentProvider) //This is the same as window.web3.currentProvider
+            this.web3 = true
             console.log('Connected with MetaMask')
         }else{
             this.currentProvider = ethers.getDefaultProvider()
+            this.web3 = false
             console.log('Using Infura/Etherscan provider', provider)
         }
         console.log('Successfully initialized DefiZap')
@@ -26,7 +28,7 @@ module.exports =  class DefiZap {
 
     // Initializes a new Zap with the given name (e.g. Lender, EthMaximalist, etc.)
     zap(zapToUse){
-        this[zapToUse] = new ZapService(zapToUse, this.currentProvider)
+        this[zapToUse] = new ZapService(zapToUse, this.currentProvider, this.web3)
         return this[zapToUse]
     }
 
