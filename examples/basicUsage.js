22fceb4be1d656d43e3abb5279935018b8069d4b
SuhailG <github@suhailg.me>
2019-12-02 05:13:43 -0700

    Update basicUsage example

--- examples/basicUsage.js
+++ examples/basicUsage.js
@@ -4,13 +4,20 @@ const DefiZap = require('../src/DefiZap');
     const defiZap = new DefiZap() //accepts a provider argument of window.web3 or is left empty for default (infura/etherscan) read-only access
 
     const lenderZap = defiZap.zap('Lender')
-    const lenderBalance = await lenderZap.getBalance()
     const lenderAddress = lenderZap.address
+    const lenderBalance = await lenderZap.getBalance()
+    
     // const lender = await lenderZap.useLenderFallback('0.01') //Invest 0.01 eth into the Lender contract (90% cDai, 10% dLETH2x)
     
     const ethMaxZap = defiZap.zap('EthMaximalist')
-    const ethMaxBalance = await ethMaxZap.getBalance()
+    const ethMaxAddress = await ethMaxZap.address
+    const ethMaxAddressBalance = await ethMaxZap.getBalance()
+
     // const ethMax = await ethMaxZap.useEthMaximalistFallback('0.01') //Invest 0.01 eth into the Eth Maximalist contract (50% dLETH2x, 50% dsWBTC)
 
-    console.log(ethMaxBalance, lenderBalance, lenderAddress,)
+    const moderateBullZap = defiZap.zap('ModerateBull')
+    const moderateBullAddress = await ethMaxZap.address
+    const moderateBullBalance = await moderateBullZap.getBalance()
+
+    console.log(lenderAddress, lenderBalance, ethMaxAddress, ethMaxAddressBalance, moderateBullAddress,moderateBullBalance)
 })()
