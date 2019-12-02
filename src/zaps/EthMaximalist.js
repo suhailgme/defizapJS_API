86f8f71cd155d470dd770eb0aa3348845dbccaf2
SuhailG <github@suhailg.me>
2019-12-01 23:26:43 -0700

    Update for use with metamask/web3 wallets

--- src/zaps/EthMaximalist.js
+++ src/zaps/EthMaximalist.js
@@ -1,39 +1,36 @@
 const ethers = require('ethers');
 const ethMaxABI = require('../../contracts/abis/EthMaximalist.json')
-const ethMaxAddress = require('../../contracts/addresses/ensMainnet.json').ETH_MAXIMALIST;
+const ethMaxAddress = require('../../contracts/addresses/mainnet.json').ETH_MAXIMALIST; //TODO: ENS names need to be resolved when using Metamask
 
-module.exports =  class EthMaximalist {
-    constructor(provider = null){
-        if(provider){
-            this.lenderContract = new ethers.Contract(ethMaxAddress, ethMaxABI, provider)
-            this.currentProvider = provider
-            console.log('Eth Maximalist Zap initialized')
-        }
+module.exports = class EthMaximalist {
+    constructor(provider, web3) {
+        this.ethMaximalistContract = new ethers.Contract(ethMaxAddress, ethMaxABI, provider)
+        this.currentProvider = provider
+        this.signer = web3 ? this.currentProvider.getSigner() : null //Get signer from Metamask or set to null for readonly access
+        console.log('Eth Maximalist Zap initialized')
     }
 
-    // Returns Eth Maximalist contract balance as a Big Number.
+     // Returns Eth Maximalist contract balance as a Big Number.
     async getBalance() {
-        return await this.lenderContract.balance()
+        return await this.ethMaximalistContract.balance()
     }
 
     // Returns owner of Eth Maximalist contract as a string
-    async getOwner(){
-        return await this.lenderContract.owner()
+    async getOwner() {
+        return await this.ethMaximalistContract.owner()
     }
 
     // Basic sending of Ether to the fallback function of the Eth Maximalist contract
     // !!! Initiates the sending of Ether !!!
-    async useEthMaximalistFallback(amount, key){
-        let pk = key // privateKey used for test transactions, metamask support forthcoming
-        let wallet = new ethers.Wallet(pk, this.currentProvider)
+    async useEthMaximalistFallback(amount) {
         let valueToInvest = ethers.utils.parseEther(amount)
         let txInfo = {
             to: ethMaxAddress,
             value: valueToInvest,
             gasLimit: 5000000,
         }
- 
-        let tx = await wallet.sendTransaction(txInfo)
+
+        let tx = await this.signer.sendTransaction(txInfo)
         return tx
 
     }
--- src/zaps/Lender.js
+++ src/zaps/Lender.js
@@ -2,14 +2,12 @@ const ethers = require('ethers');
 const lenderABI = require('../../contracts/abis/Lender.json')
 const lenderAddress = require('../../contracts/addresses/mainnet.json').LENDER; //TODO: ENS names need to be resolved when using Metamask
 
-module.exports =  class Lender {
-    constructor(provider = null, signer = null){
-        if(provider){
-            this.lenderContract = new ethers.Contract(lenderAddress, lenderABI, provider)
-            this.currentProvider = provider
-            this.signer = this.currentProvider.getSigner() //Get signer from Metamask
-            console.log('Lender Zap initialized')
-        }
+module.exports = class Lender {
+    constructor(provider, web3) {
+        this.lenderContract = new ethers.Contract(lenderAddress, lenderABI, provider)
+        this.currentProvider = provider
+        this.signer = web3 ? this.currentProvider.getSigner() : null //Get signer from Metamask or set to null for readonly access
+        console.log('Lender Zap initialized')
     }
 
     // Returns Lender contract balance as a Big Number.
@@ -18,20 +16,20 @@ module.exports =  class Lender {
     }
 
     // Returns owner of contract as a string
-    async getOwner(){
+    async getOwner() {
         return await this.lenderContract.owner()
     }
 
     // Basic sending of Ether to the fallback function of the Lender contract
     // !!! Initiates the sending of Ether !!!
-    async useLenderFallback(amount){
+    async useLenderFallback(amount) {
         let valueToInvest = ethers.utils.parseEther(amount)
         let txInfo = {
             to: lenderAddress,
             value: valueToInvest,
             gasLimit: 5000000,
         }
- 
+
         let tx = await this.signer.sendTransaction(txInfo)
         return tx
 
