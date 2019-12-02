d7e85d91d4b1bfccbb593a844c785cb500ff0903
SuhailG <github@suhailg.me>
2019-12-01 23:28:04 -0700

    Update basicUsage example to use new DefiZapJS api

--- examples/basicUsage.js
+++ examples/basicUsage.js
@@ -1,17 +1,15 @@
 const DefiZap = require('../src/DefiZap');
 
 (async() => {
-    const key = '0x372cea...' //privateKey used for test transactions, metamask support forthcoming
-    const defiZap = new DefiZap()
-    await defiZap.init() //init accepts a provider argument of 'metamask' or is left empty for default (infura/etherscan)
+    const defiZap = new DefiZap() //accepts a provider argument of window.web3 or is left empty for default (infura/etherscan) read-only access
 
     const lenderZap = defiZap.zap('Lender')
     const lenderBalance = await lenderZap.getBalance()
-    const lender = await lenderZap.useLenderFallback('0.01', key) //Invest 0.01 eth into the Lender contract (90% cDai, 10% dLETH2x)
+    const lender = await lenderZap.useLenderFallback('0.01') //Invest 0.01 eth into the Lender contract (90% cDai, 10% dLETH2x)
     
     const ethMaxZap = defiZap.zap('EthMaximalist')
     const ethMaxBalance = await ethMaxZap.getBalance()
-    const ethMax = await ethMaxZap.useEthMaximalistFallback('0.01', key) //Invest 0.01 eth into the Lender contract (50% dLETH2x, 50% dsWBTC)
+    const ethMax = await ethMaxZap.useEthMaximalistFallback('0.01') //Invest 0.01 eth into the Eth Maximalist contract (50% dLETH2x, 50% dsWBTC)
 
     console.log(ethMaxBalance, lenderBalance, lender, ethMax)
 })()
