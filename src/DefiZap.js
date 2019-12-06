3c25a05a04d0c3fddd48d23455ff90d8bd0c3cb7
SuhailG <github@suhailg.me>
2019-12-06 06:33:16 -0700

    Add getGasPrice method to DefiZap.js
    - Replace change signer method with change provider method (signer is derived from provider)

--- src/DefiZap.js
+++ src/DefiZap.js
@@ -1,14 +1,15 @@
 const ethers = require('ethers');
 const ZapService = require('./utils/ZapService')
+const gasHelper = require('./utils/gasHelper')
 
-module.exports =  class DefiZap {
+module.exports = class DefiZap {
     //Default (blank) provider is Infura homestead/mainnet and etherscan, otherwise provider should be 'window.web3' for Metamask
-    constructor(provider){
-        if(provider){
+    constructor(provider) {
+        if (provider) {
             this.currentProvider = new ethers.providers.Web3Provider(provider.currentProvider) //This is the same as window.web3.currentProvider
             this.web3 = true
             console.log('Connected with MetaMask')
-        }else{
+        } else {
             this.currentProvider = ethers.getDefaultProvider()
             this.web3 = false
             console.log('Using Infura/Etherscan provider')
@@ -16,18 +17,20 @@ module.exports =  class DefiZap {
         console.log('Successfully initialized DefiZap')
     }
 
-    // Switch signers among Metamask, ledger, etc. if needed
-    // Returns true if successful, false otherwise
-    changeSigner(signer){
-        if(this.signer !== signer){
-            this.signer = signer
+    async getGasPrice() {
+        return await gasHelper(this.currentProvider)
+    }
+
+    changeProvider(provider) {
+        if (this.currentProvider !== provider) {
+            this.currentProvider = new ethers.providers.Web3Provider(provider.currentProvider)
             return true
         }
         return false
     }
 
     // Initializes a new Zap with the given name (e.g. Lender, EthMaximalist, etc.)
-    zap(zapToUse){
+    zap(zapToUse) {
         this[zapToUse] = new ZapService(zapToUse, this.currentProvider, this.web3)
         return this[zapToUse]
     }
