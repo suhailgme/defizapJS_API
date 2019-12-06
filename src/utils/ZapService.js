49d1de97527324f13b1dd02bfa25a415337f001b
SuhailG <github@suhailg.me>
2019-12-06 06:32:18 -0700

    Add optional gas price parameter
    - If gas price is not provided, the median gas price over the past few blocks is used instead

--- src/utils/ZapService.js
+++ src/utils/ZapService.js
@@ -1,19 +1,12 @@
-const Lender = require('../zaps/Lender');
-const EthMaximalist = require('../zaps/EthMaximalist')
-const ModerateBull = require('../zaps/ModerateBull')
-const SuperSaver = require('../zaps/SuperSaver')
-const DoubleBull = require('../zaps/DoubleBull')
+const GenericZap = require('../zaps/GenericZap')
 
 const Zap = {
-    Lender,
-    EthMaximalist,
-    ModerateBull,
-    SuperSaver,
-    DoubleBull,
+
 }
 
 module.exports = class ZapService {
     constructor(zapName, provider, web3){
-        return new Zap[zapName](provider, web3)
+        if(Zap.hasOwnProperty(zapName)) return new Zap[zapName](provider, web3)
+        else return new GenericZap(zapName, provider, web3)
     }
 }
--- src/zaps/GenericZap.js
+++ src/zaps/GenericZap.js
@@ -3,10 +3,10 @@ const abiHelper = require('../utils/abiHelper')
 const addressHelper = require('../utils/addressHelper')
 
 module.exports = class GenericZap {
-    constructor(provider, web3, zap) {
+    constructor(zap, provider, web3) {
         const zapABI = abiHelper(zap)
         const zapAddress = addressHelper(zap)
-        this.zapContract = new ethers.Contract(zapAddress, zapABI, provider)
+        this.contract = new ethers.Contract(zapAddress, zapABI, provider)
         this.currentProvider = provider
         this.signer = web3 ? this.currentProvider.getSigner() : null //Get signer from Metamask or set to null for readonly access
         this.name = zap
@@ -16,25 +16,29 @@ module.exports = class GenericZap {
 
     // Returns Lender contract balance as a Big Number.
     async getBalance() {
-        return await this.zapContract.balance()
+        let balance =  await this.contract.balance()
+        return ethers.utils.formatEther(balance.toString())
     }
 
     // Returns owner of contract as a string
     async getOwner() {
-        return await this.zapContract.owner()
+        return await this.contract.owner()
     }
 
     // Basic sending of Ether to the fallback function of the Lender contract
     // !!! Initiates the sending of Ether !!!
-    async useFallback(amount) {
-        resolvedEnsAddress = await this.currentProvider.resolveName(zapAddress)
+    async useFallback(amount, price) {
+        const resolvedEnsAddress = await this.currentProvider.resolveName(this.address)
         let valueToInvest = ethers.utils.parseEther(amount)
+        let gasPriceHex
+        if(price) gasPriceHex = ethers.utils.parseUnits(price, 'gwei')
+        else gasPriceHex = await this.currentProvider.getGasPrice()
         let txInfo = {
             to: resolvedEnsAddress,
             value: valueToInvest,
             gasLimit: 5000000,
+            gasPrice: gasPriceHex
         }
-
         let tx = await this.signer.sendTransaction(txInfo)
         return tx
 
