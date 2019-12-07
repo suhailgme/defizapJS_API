e13d30ffe85dae2275c65a3deab011ff672fe61a
SuhailG <github@suhailg.me>
2019-12-07 05:18:44 -0700

    Update GenericZap to use invest functions of contract instead of relying on fallback
    - Zaps relying on Synthetix contracts will always use 1 gwei for the gas price to avoid 'Gas price above limit' errors.

--- src/zaps/GenericZap.js
+++ src/zaps/GenericZap.js
@@ -16,7 +16,7 @@ module.exports = class GenericZap {
 
     // Returns Lender contract balance as a Big Number.
     async getBalance() {
-        let balance =  await this.contract.balance()
+        let balance = await this.contract.balance()
         return ethers.utils.formatEther(balance.toString())
     }
 
@@ -25,13 +25,46 @@ module.exports = class GenericZap {
         return await this.contract.owner()
     }
 
-    // Basic sending of Ether to the fallback function of the Lender contract
+    // Sending Ether to the investment function of the contract
+    // !!! Initiates the sending of Ether !!!
+    async invest(amount, price) {
+        this.contractWithSigner = this.contract.connect(this.signer)
+        let valueToInvest = ethers.utils.parseEther(amount)
+        let gasPriceHex
+        if (price) gasPriceHex = ethers.utils.parseUnits(price, 'gwei')
+        else gasPriceHex = await this.currentProvider.getGasPrice()
+        let txInfo = {
+            value: valueToInvest,
+            gasLimit: 5000000,
+            gasPrice: gasPriceHex
+        }
+        let tx
+        switch (this.name) {
+            case 'Lender':
+                tx = await this.contractWithSigner.SafeNotSorryZapInvestment(txInfo)
+                break
+            case 'EthMaximalist':
+                tx = await this.contractWithSigner.ETHMaximalistZAP(txInfo)
+                break
+            case 'ModerateBull':
+                txInfo.gasPrice = ethers.utils.parseUnits('1.0', 'gwei')
+                tx = await this.contractWithSigner.LetsInvest(txInfo)
+                break
+            default:
+                tx = await this.contractWithSigner.LetsInvest(txInfo)
+                break
+        }
+        return tx
+
+    }
+
+    // Basic sending of Ether to the fallback function of the contract
     // !!! Initiates the sending of Ether !!!
     async useFallback(amount, price) {
         const resolvedEnsAddress = await this.currentProvider.resolveName(this.address)
         let valueToInvest = ethers.utils.parseEther(amount)
         let gasPriceHex
-        if(price) gasPriceHex = ethers.utils.parseUnits(price, 'gwei')
+        if (price) gasPriceHex = ethers.utils.parseUnits(price, 'gwei')
         else gasPriceHex = await this.currentProvider.getGasPrice()
         let txInfo = {
             to: resolvedEnsAddress,
